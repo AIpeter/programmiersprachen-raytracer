0e45c2cd1f16ba15a995bff6fb73bd4c944c62fc
Anne Peter <anne_peter@live.de>
2015-06-17 16:31:09 +0200

    add class Shape

---
+++ framework/shape.hpp
@@ -0,0 +1,10 @@
+#ifndef BUW_SHAPE_HPP
+#define BUW_SHAPE_HPP
+
+class Shape
+{
+  public:
+    virtual float area() const = 0; // pure virtual
+    virtual float volume() const = 0; // pure virual
+
+};
