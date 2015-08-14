61f81b96c2217a736bbe0c4d909c8a80c9395d54
Anne Peter <anne_peter@live.de>
2015-08-14 14:36:17 +0200

    Fix camera

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -1,8 +1,8 @@
 #include "camera.hpp"
 
 Camera::Camera() :
-  name_{cam},
-  fov_x_{45}
+  name_{"cam"},
+  fov_x_{45},
   position_{0, 0, 0},
   direction_{0, 0, -1}, // camera looks in direction of negative z-axis
   cam_up_{0, 1, 0}
@@ -15,7 +15,7 @@ Camera::Camera(std::string const& name, double const fov_x,
                 glm::vec3 const& position, glm::vec3 direction,
                 glm::vec3 up) :
   name_{name},
-  fov_x_{fov_x}
+  fov_x_{fov_x},
   position_{position},
   direction_{direction},
   cam_up_{up}
@@ -24,17 +24,17 @@ Camera::Camera(std::string const& name, double const fov_x,
     cam_up_ = glm::cross(cam_right_, direction_);
   }
 
-std::string const& Camera::getname() const
+std::string const& Camera::getName() const
 {
 	return name_;
 }
 
-double const& Camera::getfov_x() const
+double const& Camera::getFov_x() const
 {
   return fov_x_;
 }
 
-glm::vec3 const& Camera::getpostion() const
+glm::vec3 const& Camera::getPostion() const
 {
 	return position_;
 }
