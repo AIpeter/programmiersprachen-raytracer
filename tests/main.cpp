b43374992b1799d1d675a0ab66631ccc1e0b50dc
Lars Meyer <lars.meyer@uni-weimar.de>
2015-08-14 16:07:29 +0200

    Add unsuccessful test case for composite.

--- tests/main.cpp
+++ tests/main.cpp
@@ -42,14 +42,19 @@ TEST_CASE("jetzt wird's ernst", "[renderer]") {
   Scene* scene = loadSDF("sdfdatei.sdf");
   scene->render.render(scene->shapes);
 }
-
+/*
 TEST_CASE("composite", "[composite]") {
   Material m{"Testmaterial", Color{1,0,0}, Color{0,1,0}, Color{0,0,1}, 15};
-  Box b{glm::vec3{0.0, 0.0, 0.0}, glm::vec3{1.0, 1.0, 1.0}, "testbox", m};
-  Sphere s{};
+  auto b = std::make_shared<Box>(glm::vec3{0.0, 0.0, 0.0}, glm::vec3{1.0, 1.0, 1.0}, "testbox", m);
+  auto s = std::make_shared<Sphere>(glm::vec3{1.0, 1.0, 1.0}, 1, "testsphere", m);
   Composite c{b, s};
+  // std::cout << c << std::endl;
+  Ray r{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}};
+  float t = 0;
+  REQUIRE(c.intersect(r, t) == true);
+  std::cout << c.volume() << std::endl;
 }
-
+*/
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
