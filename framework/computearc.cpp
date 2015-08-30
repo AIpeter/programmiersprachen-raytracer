d3c870983889dd8e838d6635d2dd565bad00e607
Lars Meyer <lars.meyer@uni-weimar.de>
2015-08-30 12:33:03 +0200

    Add preliminary specular lighting.

--- framework/computearc.cpp
+++ framework/computearc.cpp
@@ -128,15 +128,86 @@ float computeDiffuseArc(Box const& box, float & d, Ray const& r, Light const& li
   float n_length = glm::length(n);
   float l_length = glm::length(l.direction);
   float diffuseArc = (std::max(glm::dot(n, l.direction), 0.0f))/(n_length*l_length);
-  if (!(glm::isnan(diffuseArc))) {
-    std::cout << "diffusearc: " << diffuseArc << std::endl;
-  }
   return diffuseArc;
 }
 
 float computeSpecularArc(Box const& box, float & d, Ray const& r, Light const& light)
 {
+  glm::vec3 n;
+  glm::vec3 surfacePoint{d*r.direction};
+  //std::cout << "d: " << d << std::endl;
+  // std::cout << surfacePoint.x << ", " << surfacePoint.y << ", " << surfacePoint.z << std::endl;
+  Ray l{surfacePoint, surfacePoint - light.getposition()};
 
+//test x plane
+  if( surfacePoint.y >= box.min().y &&
+      surfacePoint.y <= box.max().y &&
+      surfacePoint.z >= box.min().z &&
+      surfacePoint.z <= box.max().z)
+  {
+    if (surfacePoint.x < box.max().x)
+    {//if surfacepoint on x-min-plane glm::vec3 n{-1, 0, 0}
+      n = glm::normalize(glm::vec3{box.min().x, box.max().y, box.max().z}-box.max());
+    }
+    if (surfacePoint.x > box.min().x)
+    { //if surfacepoint on x-max-plane glm::vec3 n{1, 0, 0}
+      n = glm::normalize(box.max() - glm::vec3{box.min().x, box.max().y, box.max().z});
+    }
+  }
+  //test y plane
+  if( surfacePoint.x <= box.max().x &&
+    surfacePoint.x >= box.min().x &&
+    surfacePoint.z <= box.max().z &&
+    surfacePoint.z >= box.min().z)
+  {
+    if(surfacePoint.y < box.max().y)
+    { //if surfpoint on y-min-plane:
+      n = glm::normalize(glm::vec3{box.max().x, box.min().y, box.max().z} - box.max());
+    }
+    if(surfacePoint.y > box.min().y)
+    { //if surfpoint on y-max plane
+      n = glm::normalize(box.max() - glm::vec3{box.max().x, box.min().y, box.max().z});
+    }
+    //std::cout << "i'm here" << std::endl;
+    
+  }
+
+//tests z plane
+  if (surfacePoint.x <= box.max().x &&
+    surfacePoint.x >= box.min().x &&
+    surfacePoint.y <= box.max().y &&
+    surfacePoint.y >= box.min().y)
+  {
+    if(surfacePoint.z < box.max().z)
+    {
+      //if on z min plane:
+      n = glm::normalize(glm::vec3{box.max().x, box.max().y, box.min().z} - box.max());
+    }
+    if(surfacePoint.z > box.min().z)
+    { //if on z max plane
+      n = glm::normalize(box.max() - glm::vec3{box.max().x, box.max().y, box.min().z});
+    }
+  }
+
+
+  // std::cout << n.x << n.y << n.z << std::endl;
+  glm::vec3 s = (2 * std::max(glm::dot(n, l.direction), 0.0f))*n - l.direction;
+  s = glm::normalize(s);
+  auto c = glm::normalize(r.direction);
+  float specularCos = glm::dot(s, c);
+  float specularArc = glm::acos(specularCos);
+  float specularSin = glm::sin(specularArc);
+  // std::cout << specularArc << std::endl;
+  if(specularSin > 0) 
+  {
+    std::cout << "specularCos: " << specularCos << std::endl;
+    return specularCos;
+  }
+  else 
+  {
+    std::cout << "specularCos: " << (0 - specularCos) << std::endl;
+    return (0 - specularCos);
+  }
 }
 
 float computeDiffuseArc(Triangle const& triangle, float & d, Ray const& r, Light const& light)
