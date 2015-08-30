9132b97a4e22ffebde65b46bb1e4a2501e55f055
Lars Meyer <lars.meyer@uni-weimar.de>
2015-08-30 20:04:02 +0200

    Another unsuccessful attempt at fixing lighting.

--- framework/computearc.cpp
+++ framework/computearc.cpp
@@ -3,29 +3,14 @@
 float computeDiffuseArc(Hit const& hit, Light const& light)
 {
   glm::vec3 l_direction = glm::normalize(hit.intersectionPoint - light.getposition());
-  float n_length = glm::length(hit.normal);
-  float l_length = glm::length(l_direction);
-  return (std::max(glm::dot(hit.normal, l_direction), 0.0f)/(n_length * l_length));
+  return std::max(glm::dot(hit.normal, l_direction), 0.0f);
 }
 
 float computeSpecularArc(Hit const& hit, Ray const& r, Light const& light)
-{
-  // std::cout << "hit.intersectionPoint: " << glm::to_string(hit.intersectionPoint) << std::endl;
-  // std::cout << "light.getposition(): " << glm::to_string(light.getposition()) << std::endl; 
+{ 
   glm::vec3 l_direction = glm::normalize(hit.intersectionPoint - light.getposition());
-  glm::vec3 s = (2 * std::max(glm::dot(hit.normal, l_direction), 0.0f))*hit.normal - l_direction;
+  glm::vec3 s = glm::reflect(l_direction, hit.normal);
   s = glm::normalize(s);
   auto c = glm::normalize(r.direction);
-  // std::cout << "s: " << glm::to_string(s) << std::endl;
   float specularCos = glm::dot(s, c);
-  float specularArc = glm::acos(specularCos);
-  float specularSin = glm::sin(specularArc);
-  if(specularSin > 0) 
-  {
-    return specularCos;
-  }
-  else 
-  {
-    return (0 - specularCos);
-  }
 }
