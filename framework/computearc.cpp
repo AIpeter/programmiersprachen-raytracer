010882ee42a796bec8abe4f83e990cba3aa8643a
Theresa Elstner <theresa.elstner@gmail.com>
2015-08-30 22:53:04 +0200

    fix specular light

--- framework/computearc.cpp
+++ framework/computearc.cpp
@@ -2,16 +2,16 @@
 
 float computeDiffuseArc(Hit const& hit, Light const& light)
 {
-  glm::vec3 l_direction = glm::normalize(hit.intersectionPoint - light.getposition());
+  glm::vec3 l_direction = glm::normalize(light.getposition() - hit.intersectionPoint);
   return std::max(glm::dot(hit.normal, l_direction), 0.0f);
 }
 
 float computeSpecularArc(Hit const& hit, Ray const& r, Light const& light)
 { 
-  glm::vec3 l_direction = glm::normalize(hit.intersectionPoint - light.getposition());
-  glm::vec3 s = glm::reflect(l_direction, hit.normal);
+  glm::vec3 l_direction = glm::normalize(light.getposition() - hit.intersectionPoint);
+  glm::vec3 s = glm::reflect(l_direction, glm::normalize(hit.normal));
   s = glm::normalize(s);
   auto c = glm::normalize(r.direction);
-  float specularCos = glm::dot(s, c);
+  float specularCos = std::max(glm::dot(s, c), 0.0f);
   return specularCos;
 }
